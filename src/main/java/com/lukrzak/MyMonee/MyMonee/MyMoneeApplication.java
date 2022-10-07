609f8c4a6dc0a87d573aa9eeccb9408741759f72
Lukasz Nowakowski <lukrzak@wp.pl>
2022-10-07 20:59:39 +0200

    Initial commit

---
+++ src/main/java/com/lukrzak/MyMonee/MyMonee/MyMoneeApplication.java
@@ -0,0 +1,13 @@
+package com.lukrzak.MyMonee.MyMonee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoneeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukrzak/MyMonee/MyMonee/MyMoneeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukrzak.MyMonee.MyMonee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoneeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
