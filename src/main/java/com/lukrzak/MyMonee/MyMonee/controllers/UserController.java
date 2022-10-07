c6470ace5d44566e5c5cd66891cd8932c7c72019
Lukasz Nowakowski <lukrzak@wp.pl>
2022-10-07 21:45:28 +0200

    Added mapping, methods body and response status

--- src/main/java/com/lukrzak/MyMonee/MyMonee/controllers/UserController.java
+++ src/main/java/com/lukrzak/MyMonee/MyMonee/controllers/UserController.java
@@ -4,10 +4,10 @@ import com.lukrzak.MyMonee.MyMonee.models.User;
 import com.lukrzak.MyMonee.MyMonee.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 public class UserController {
 
     private final UserService userService;
@@ -17,11 +17,15 @@ public class UserController {
         this.userService = userService;
     }
 
-    public void addUser(User user){
-
+    @PostMapping("/users")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void addUser(@RequestBody User user){
+        userService.addUser(user);
     }
 
+    @PostMapping("/users/change-balance")
+    @ResponseStatus(code = HttpStatus.OK)
     public void changeUserBalance(User user, float changedValue){
-
+        userService.changeUserBalance(user, changedValue);
     }
 }
