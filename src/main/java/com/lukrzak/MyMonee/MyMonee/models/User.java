9a79a394c56302c9b813285f01cefb688431ecbe
Lukasz Nowakowski <lukrzak@wp.pl>
2022-10-07 21:28:03 +0200

    Added user model

---
+++ src/main/java/com/lukrzak/MyMonee/MyMonee/models/User.java
@@ -0,0 +1,25 @@
+package com.lukrzak.MyMonee.MyMonee.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String surname;
+    private float balance;
+
+}
