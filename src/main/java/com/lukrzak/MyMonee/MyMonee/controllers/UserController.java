3b8a845c1ed90bc1e8cde0a462f958d96393a48e
Lukasz Nowakowski <lukrzak@wp.pl>
2022-10-07 21:41:01 +0200

    Dependency injection of userSerivce at UserController

--- src/main/java/com/lukrzak/MyMonee/MyMonee/controllers/UserController.java
+++ src/main/java/com/lukrzak/MyMonee/MyMonee/controllers/UserController.java
@@ -1,2 +1,27 @@
-package com.lukrzak.MyMonee.MyMonee.controllers;public class UserController {
+package com.lukrzak.MyMonee.MyMonee.controllers;
+
+import com.lukrzak.MyMonee.MyMonee.models.User;
+import com.lukrzak.MyMonee.MyMonee.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void addUser(User user){
+
+    }
+
+    public void changeUserBalance(User user, float changedValue){
+
+    }
 }
