2149f4ca9e5d0c8e1ad8cce28c2b20a397e704b3
Lukasz Nowakowski <lukrzak@wp.pl>
2022-10-10 21:16:19 +0200

    Added Expense model

--- src/main/java/com/lukrzak/MyMonee/MyMonee/models/Expense.java
+++ src/main/java/com/lukrzak/MyMonee/MyMonee/models/Expense.java
@@ -1,2 +1,41 @@
-package com.lukrzak.MyMonee.MyMonee.models;public class Expense {
+package com.lukrzak.MyMonee.MyMonee.models;
+
+import com.lukrzak.MyMonee.MyMonee.Categories;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "expenses")
+public class Expense {
+
+    public Expense(String name, Categories category, String model, String placeOfPurchase, double price, int quantity, Date date) {
+        this.name = name;
+        this.category = category;
+        this.model = model;
+        this.placeOfPurchase = placeOfPurchase;
+        this.price = price;
+        this.quantity = quantity;
+        this.date = date;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Categories category;
+    private String model;
+    private String placeOfPurchase;
+    private double price;
+    private int quantity;
+    private Date date;
 }
