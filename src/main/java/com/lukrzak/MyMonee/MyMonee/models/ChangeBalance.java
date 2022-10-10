fefeae46f13f950e8593ba82a883b752053121da
Lukasz Nowakowski <lukrzak@wp.pl>
2022-10-10 19:03:16 +0200

    Added Request body for changing balance

--- src/main/java/com/lukrzak/MyMonee/MyMonee/models/ChangeBalance.java
+++ src/main/java/com/lukrzak/MyMonee/MyMonee/models/ChangeBalance.java
@@ -1,2 +1,13 @@
-package com.lukrzak.MyMonee.MyMonee.models;public class ChangeBalance {
+package com.lukrzak.MyMonee.MyMonee.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangeBalance {
 }
